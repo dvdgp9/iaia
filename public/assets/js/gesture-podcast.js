0d948b13f51d2c95d1483f850f85fc1ab2013ed7
David Gutiérrez <dvdgp93@gmail.com>
2025-12-23 10:59:26 +0100

    Ajustes estilo podcast 2

--- public/assets/js/gesture-podcast.js
+++ public/assets/js/gesture-podcast.js
@@ -409,6 +409,9 @@
     return escaped
       .replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>')
       .replace(/__(.+?)__/g, '<strong>$1</strong>')
+      // Italic with single * or _ (avoid matching bold)
+      .replace(/\*(?!\*)(.+?)\*/g, '<em>$1</em>')
+      .replace(/_(?!_)(.+?)_/g, '<em>$1</em>')
       .replace(/\n/g, '\n');
   }
 
